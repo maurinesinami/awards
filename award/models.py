9d606f2562e2e6730e8aa8e150b27d251fbc4cae
maurinenami <maurinenami>
2019-06-30 17:47:06 +0300

    cretaed the profiles model importe the model in views.py and added postgres to the project in settings.py

--- award/models.py
+++ award/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    profile_photo= models.ImageField(upload_to = 'awards/', blank = True)
+    bio=models.CharField(max_length =50)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    contact=models.CharField(max_length =50)
+    def __str__(self):
+        return self.bio
+    def save_profile(self):
+        self.save()
--- award/views.py
+++ award/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from . models import  Profile
+from django.contrib.auth.models import User
 @login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request,'welcome.html')
